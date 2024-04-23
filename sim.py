416e7fb05e4a77cc3f732edeabc258a8ffc0382e
MustafaSari1 <hacimustafasari46@gmail.com>
2024-04-23 17:40:27 +0300

    Update sim.py

--- sim.py
+++ sim.py
@@ -7,20 +7,72 @@ SIM_RATE = 400
 TIME_STEP_S = 1.0 / SIM_RATE
 GRAVITY_M_SS = 9.80665
 
+MASS_KG = 1.0
+INERTIA_KGM2 = 0.01
+
 time_now = 0
 connected = False
+
+# Initial physics state
+velocity_m_s = 0
+position_m = 0
+angular_velocity_rad_s = 0
+orientation_rad = 0
 
 # Initialize the simulation / Reset physics
 def init():
     global time_now
     time_now = 0
+
+    global velocity_m_s
+    velocity_m_s = 0
+
+    global position_m
+    position_m = 0
+
+    global angular_velocity_rad_s
+    angular_velocity_rad_s = 0
+
+    global orientation_rad
+    orientation_rad = 0
     
     # Reset position and orientation
 
 def calculate_new_state(pwm_values):
     # Calculate the new state of the physics
-    # Return timestamp, gyro, accel, position, attitude, velocity
-    return 0
+    # Return timestamp, gyro(angular vel), accel, position, attitude, velocity
+
+    global time_now
+    time_now += TIME_STEP_S
+
+    motors = pwm_values[0:4]
+    num_motors = len(motors)
+    thrusts = [0 for _ in range(num_motors)]
+    torque = [0 for _ in range(num_motors)]
+
+    motor_directions = [1, 1, -1, -1]
+
+    for n in range(num_motors):
+        thrusts[n] = motors[n]
+        torque[n] = motor_directions[n] * motors[n]
+
+    total_thrust = sum(thrusts)
+    total_torque = sum(torque)
+
+    linear_acceleration_m_ss = total_thrust / MASS_KG
+    angular_acceleration_rad_ss = total_torque / INERTIA_KGM2
+
+    global velocity_m_s
+    global position_m
+    velocity_m_s += linear_acceleration_m_ss * TIME_STEP_S
+    position_m += velocity_m_s * TIME_STEP_S
+
+    global angular_velocity_rad_s
+    global orientation_rad
+    angular_velocity_rad_s += angular_acceleration_rad_ss * TIME_STEP_S
+    orientation_rad += angular_velocity_rad_s * TIME_STEP_S
+
+    return time_now, angular_acceleration_rad_ss, linear_acceleration_m_ss, position_m, orientation_rad, velocity_m_s
 
 # Create a socket object for UDP communication and bind it to port 9002 on the local machine
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -91,20 +143,20 @@ while True:
     frame_count += 1
 
     # Get outputs from the physics
-    physics_time_s, gyro_body, accel_body, position_earth, attitude, velocity_earth = calculate_new_state(pwm_values)
+    physics_time_s, gyro_body_rad_s, accel_body_m_ss, position_earth_m, attitude_rad, velocity_earth_m_s = calculate_new_state(pwm_values)
 
     # Convert outputs to JSON
     IMU_format = {
-        "gyro": gyro_body,
-        "accel": accel_body
+        "gyro": gyro_body_rad_s,
+        "accel": accel_body_m_ss
     }
 
     JSON_format = {
         "timestamp": physics_time_s,
         "imu": IMU_format,
-        "position": position_earth,
-        "attitude": attitude,
-        "velocity": velocity_earth
+        "position": position_earth_m,
+        "attitude": attitude_rad,
+        "velocity": velocity_earth_m_s
     }
 
     JSON_string = "\n" + json.dumps(JSON_format, separators=(',', ':')) + "\n"
